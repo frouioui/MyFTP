6b06eb9509a6f041ff4bf41e66d109d3155cb4a6
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-02 19:19:43 +0200

    [MOD] - Modification of the travis file, addition of pre-prod

---
+++ ci/script/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd $APPLICATION_DIRECTORY_DOCKER
+
+echo "oops"
---
+++ ci/script/deploy_documentation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd $APPLICATION_DIRECTORY_DOCKER
+
+echo "hello" > index.html
+
+# dnf install doxygen -y
+# make doxygen
+# rm -Rf Include/ Src/ Tests/ .github/ Makefile
+# mv html/* .
+# rm -Rf latex
