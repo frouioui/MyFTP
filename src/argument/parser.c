db110d32e03ec7142a51d52d9b4c7b07e635ab5c
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-12 19:32:32 +0200

    [HOTFIX] - error handling

--- src/argument/parser.c
+++ src/argument/parser.c
@@ -7,6 +7,9 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "argument.h"
 #include "string_parser.h"
 
@@ -19,6 +22,14 @@ static bool is_helper_flag(char *flag)
     return (false);
 }
 
+static bool is_path_dir(const char *path)
+{
+    struct stat path_stat;
+
+    stat(path, &path_stat);
+    return !S_ISREG(path_stat.st_mode);
+}
+
 argument_t parse_argument(int argc, char *argv[])
 {
     argument_t args = {helper: false, valid: false, port: 21, path: ""};
@@ -29,7 +40,11 @@ argument_t parse_argument(int argc, char *argv[])
     } else if (argc == 3 && is_number(argv[1]) == true) {
         args.port = atoi(argv[1]);
         args.path = strdup(argv[2]);
-        args.valid = true;
+        if (is_path_dir(args.path) == false) {
+            args.valid = false;
+        } else {
+            args.valid = true;
+        }
     }
     return (args);
 }
